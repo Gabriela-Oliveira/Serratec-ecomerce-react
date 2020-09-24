757a9ed0d05a0527703a35ea13ea5cf5c64692ca
PauloEduardoLucas <dudu.magnificat@gmail.com>
2020-09-24 12:23:10 -0300

    comentando erro para ser resolvido pelo cicero mais tarde

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import GlobalStyle from './styles/globalStyles';
-function App() {
-  return (
-    <>
-    <GlobalStyle />
-    <h1>Hello Boys and Girl</h1>
 
-=======
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+// import GlobalStyle from './styles/global';
 
 function App() {
   return (
@@ -18,7 +11,7 @@ function App() {
       <BrowserRouter>
           <Routes />
       </BrowserRouter>
->>>>>>> 514f456647197cce0218428fe3379c439ff40355
+      {/* <GlobalStyle /> */}
     </>
   );
 }
