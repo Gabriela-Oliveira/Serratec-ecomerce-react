56847ea1ae33f9378bbff2c31f08a8b516768e4a
cicero romao <cicerotransformice@gmail.com>
2020-09-24 12:46:27 -0300

    subindo src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-import GlobalStyle from './styles/globalStyles';
+
+import GlobalStyles from './styles/global';
 function App() {
   return (
     <>
-    <GlobalStyle />
     <h1>Hello Boys and Girl</h1>
 
-=======
-import { BrowserRouter } from 'react-router-dom';
-
-import Routes from './routes';
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-          <Routes />
-      </BrowserRouter>
->>>>>>> 514f456647197cce0218428fe3379c439ff40355
+    <GlobalStyles />
     </>
-  );
+  )
 }
 
 export default App;
--- src/pages/Funcionario/index.js
+++ src/pages/Funcionario/index.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react-router-dom';
+import React , { useState, useCallback , useEffect } from 'react-router-dom';
 
 import api from '../../services/api';
 import { Link } from 'react-router-dom';
@@ -7,4 +7,85 @@ import './styles';
 
 const Funcionario = () => {
 
+    const [ mostrarCliente, setMostrarCliente ] = useState([]);
+    const [ mostrarClienteID, setMostrarClienteID ] = useState({});
+    const [ mostrarFuncionario, setMostrarFuncionario ] = useState({});
+    const [ nomeFuncionario, setNomeFuncionario ] = useState('');
+    const [ cpfFuncionario, setCpfFuncionario ] = useState('');
+    const [ erroMensagem,  setErroMensagem ] = useState('');
+    
+
+       const mostrarClientes = useCallback(
+           async () => {
+               try {
+                   const response = await api.get(`/funcionario`);
+                   setMostrarCliente(response.data);
+
+                   console.log("Clientes", response.data);
+
+               }catch (error){
+                console.log("Erro Cliente", error);
+                setErroMensagem(error);
+               }
+           },[]
+       );
+
+       const mostrarClienteId = useCallback (
+
+        async (idCliente) => {
+            try {
+                const resposta = await api.get(`/funcionario/${idCliente}`);
+                setMostrarClienteID(resposta.data);
+                console.log("Cliente Achado por Id ", resposta.data);
+
+            } catch (error) {
+                console.log("Error ao tentar achar cliente pelo ID ");
+                setErroMensagem(error);
+            }
+        },[]
+       );
+
+       useEffect(() => {
+        mostrarClientes()
+       },[mostrarClientes])
+
+       const mostrarTodosFuncionarios = useCallback(
+
+            async (idFuncionario) => {
+                try {
+                    const resposta = await api.get(`/funcionario/${idFuncionario}`);
+                    console.log("Funcionario encontrado com sucesso");
+                } catch (error) {
+                    console.log("Erro ao encontrar Funcionario");
+                    erroMensagem(error);
+                }
+            },[]
+
+       )
+
+       const adcionarFuncionario = useCallback(
+           async (e) => {
+               e.preventDefault();
+
+               const parametros = {
+                cpf: cpfFuncionario,
+                nome: nomeFuncionario
+              } 
+
+              if(!nomeFuncionario && !cpfFuncionario){
+                setErroMensagem("Nome ou CPF vazios !")
+                return;
+              }
+              setErroMensagem('');
+
+              try {
+                  await api.post(`/funcionario`, parametros);
+                  mostrarFuncionario();
+
+              } catch (error) {
+                  
+              }
+           },[]
+
+       )
 }
