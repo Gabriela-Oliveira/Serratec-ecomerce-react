d41dc360270c3828d8966965071072afad2d4206
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-09-24 10:28:48 -0300

    Criando carrinho

---
+++ src/pages/Carrinho/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useCallback, useMemo } from 'react';
+
+
+const Carrinho = () => {
+    const [items, setItems] = useState([]);
+    const [pedido, setPedido] = useState({});
+
+    // const adicionarItems = (item) => {
+    //     setItems([...items, item]);
+    // }
+
+    // adicionarItems('pedido');
+    // adicionarItems('pedido2');
+    // adicionarItems('pedido3');
+    // adicionarItems('pedido4');
+    // adicionarItems('pedido5');
+
+
+    return(
+        <>
+        <h1>Teste</h1>
+        {
+            items.map(item => (<h1>{item}</h1>))
+        }
+        </>
+    )
+}
+
+export default Carrinho;
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Router } from 'react-router-dom';
 
 import Login from '../pages/Login';
+import Carrinho from '../pages/Carrinho';
 const Router = () => (
 
         <BrowserRouter>
         <Switch>
             <Router exact path="/" component={Login} />
+            <Router path="/carrinho" component={Carrinho} />
         </Switch>
         </BrowserRouter>
 );
