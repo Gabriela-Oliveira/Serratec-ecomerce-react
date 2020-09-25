58c6f7750315cf54c5c9e8b817b4b4a41c44c4de
cicero romao <cicerotransformice@gmail.com>
2020-09-25 08:40:19 -0300

    Alterações do funcionario

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,12 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
-<<<<<<< HEAD
-import Cadastro_Cliente from '../pages/Cadastro_Cliente';
-import Cadastro_Endereco from '../pages/Cadastro_Endereco';
-import Cadastro_Funcionario from '../pages/Cadastro_Funcionario';
-import Login_Cliente from '../pages/Login_Cliente';
-import Login_Funcionario from '../pages/Login_Funcionario';
-import Funcionario from '../pages/Funcionario';
-=======
 import Cadastro_Cliente from '../pages/Cadastro/Cadastro_Cliente';
 import Cadastro_Endereco from '../pages/Cadastro/Cadastro_Endereco';
 import Cadastro_Funcionario from '../pages/Cadastro/Cadastro_Funcionario';
 import Login_Cliente from '../pages/Login/Login_Cliente';
 import Login_Funcionario from '../pages/Login/Login_Funcionario';
->>>>>>> f62053b6e162467dec303354678d8466b1ec605c
+import Funcionario from '../pages/Funcionario';
 
 const Routes = () => (
 
@@ -32,6 +24,7 @@ const Routes = () => (
             <Route path="/Lcliente" component={Login_Cliente} />
             <Route path="/Lfuncionario" component={Login_Funcionario} />
             <Route path="/funcionario" component={Funcionario} />
+            
         </Switch>
 
     </BrowserRouter>
