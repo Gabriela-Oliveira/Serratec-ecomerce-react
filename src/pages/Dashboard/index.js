624daa3dbdbda97ad87187bdf35410dcefe2152d
PauloEduardoLucas <dudu.magnificat@gmail.com>
2020-09-27 21:11:06 -0300

    Subindo estilização do logo com autenticação

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { Body, Area_Cliente, Area_Funcionario } from './styles';
+import { Body, Container } from "./styles";
+
+import logo from '../../assets/Logo1.png';
 
 const Dashboard = () => {
-    return(
-        <Body>
-            <Area_Cliente>
-                <div class="container">
-                    <h1>Área do cliente</h1>
-                    <Link to='/Ccliente' >Cadastro</Link>
-                    <Link to='/Lcliente' >Log in</Link>
-                </div>
-            </Area_Cliente>
-            <Area_Funcionario>
-                <div class="container">
-                        <h1>Área do funcionario</h1>    
-                        <Link to='/Cfuncionario' >Cadastro</Link>
-                        <Link to='/Lfuncionario' >Log in</Link>
-                </div>
-            </Area_Funcionario>
-        </Body>
-    );
-}
+  return (
+    <Body>
+      <Container>
+          <header>
+            <img src={logo}></img>
+          </header>
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu1">
+              Cliente
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu2">
+              Funcionario
+            </a>
+          </li>
+        </ul>
+
+        <div class="tab-content">
+          <div class="tab-pane container active" id="menu1">
+            <div class="container">
+              <h3>Área do cliente</h3>
+              <Link to="/Ccliente">Cadastro</Link>
+              <Link to="/Lcliente">Log in</Link>
+            </div>
+          </div>
+          <div class="tab-pane container fade" id="menu2">
+            <div class="container">
+              <h3>Área do funcionario</h3>
+              <Link to="/Cfuncionario">Cadastro</Link>
+              <Link to="/Lfuncionario">Log in</Link>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Body>
+  );
+};
 
 export default Dashboard;
